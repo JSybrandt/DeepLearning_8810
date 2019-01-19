b51bd41f54ae7fc04ddb0e1ae0028e93d280f859
Justin Sybrandt <jsybran@jcloud.clemson.edu>
2019-01-19 17:51:24 -0500

    Adds initial files for main

---
+++ cyberbully_detector/__main__.py
@@ -0,0 +1,4 @@
+if __name__ == "__main__":
+  print("Run as script!")
+else:
+  print("Loaded as Module!")
---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+setup(name='cyberbully_detector',
+      version='0.1',
+      py_modules=['cyberbully_detector'],
+)
